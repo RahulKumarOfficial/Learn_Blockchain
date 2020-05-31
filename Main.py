3b195447f299408f0037ccdfd700bdee4c6a524c
Rahul Kumar <Rahulkr9622@gmail.com>
2020-05-31 21:25:08 +0530

    Follow styleguide

--- Main.py
+++ Main.py
@@ -11,8 +11,22 @@ from flask import Flask, jsonify
 
 # Part 1. Build a Blockchain
 
+"""
+Genesis block is marked by create_block() function
+with previous hash as '0'
+"""
+
 
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.create_block(proof = 1, previous_hash = '0')
+        self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash,
+                 }
+        self.chain.append(block)
+        return block
